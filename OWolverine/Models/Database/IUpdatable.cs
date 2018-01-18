02aeccf6b507b2c0f104fbbea53fdaaa82fba064
Eric Lam <ericlamyauping@gmail.com>
2018-01-18 18:25:44 +1000

    Database updatable object

---
+++ OWolverine/Models/Database/IUpdatable.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OWolverine.Models.Database
+{
+    public interface IUpdatable
+    {
+        void Update(IUpdatable obj);
+    }
+}
