1dcafca8a460e26a591b1faec3acc98562816c0a
Eric Lam <ericlamyauping@gmail.com>
2018-01-25 23:11:01 +1000

    do not seed database

--- OWolverine/Program.cs
+++ OWolverine/Program.cs
@@ -17,20 +17,6 @@ namespace OWolverine
         public static void Main(string[] args)
         {
             var host = BuildWebHost(args);
-            //Seed the database
-            using (var scope = host.Services.CreateScope())
-            {
-                var services = scope.ServiceProvider;
-                try
-                {
-                    var context = services.GetRequiredService<ApplicationDbContext>();
-                }
-                catch (Exception ex)
-                {
-                    var logger = services.GetRequiredService<ILogger<Program>>();
-                    logger.LogError(ex, "An error occurred while seeding the database.");
-                }
-            }
             host.Run();
         }
 
